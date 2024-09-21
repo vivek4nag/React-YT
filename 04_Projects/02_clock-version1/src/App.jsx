680e7d39168b499f7d1c81207129be5eb27dfff6
viek4nag <vivek4nag@gmail.com>
2024-09-21 15:44:10 +0530

    basic clock

---
+++ 04_Projects/02_clock-version1/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import ClockHeading from './components/ClockHeading'
+import ClockSlogan from './components/ClockSlogan'
+import CurrentTime from './components/CurrentTime'
+
+function App() {
+  
+
+  return (
+    <center>
+      <ClockHeading/>
+      <ClockSlogan/>
+      <CurrentTime/>
+
+    </center>
+  )
+}
+
+export default App
---
+++ 04_Projects/02_clock-version1/src/components/ClockSlogan.jsx
@@ -0,0 +1,7 @@
+let ClockSlogan = () => {
+    return <p className="lead">
+        This is a clock showing the Indian Standard time
+    </p>
+}
+
+export default ClockSlogan
